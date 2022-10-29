87a89989ee967ea40850fca11e734d1c2ec8803d
Abhinab <abhinabacharya303@gmail.com>
2022-10-29 15:04:04 -0400

    change lr

--- gcn_with_snlt_features.py
+++ gcn_with_snlt_features.py
@@ -129,8 +129,8 @@ class GCN(nn.Module):
         super(GCN, self).__init__()
 
         self.gc1 = GraphConv(nfeat, nhid1, norm='both', weight=True, bias=True)
-        # self.gc2 = GraphConv(nhid1, nhid2, norm='both', weight=True, bias=True)
-        self.gc2 = GraphConv(nhid1, nclass, norm='both', weight=True, bias=True)
+        self.gc2 = GraphConv(nhid1, nhid2, norm='both', weight=True, bias=True)
+        # self.gc2 = GraphConv(nhid1, nclass, norm='both', weight=True, bias=True)
         # self.gc3 = GraphConvolution(nhid2, nclass)
         self.dropout = dropout
         self.final_liner = nn.Linear(in_features=nhid2, out_features=nclass)
@@ -202,10 +202,9 @@ dd = embeddings[0].shape[0] - feature_matrix.shape[1]
 f2p = np.pad(f2, ((0, 0), (0, dd)), 'constant')
 new_emb = np.concatenate((f1, f2p), axis=0)
 
-features = sp.csr_matrix(feature_matrix)
-# embeddings = np.array(embeddings)
-# embeddings = new_emb
-# features = sp.csr_matrix(embeddings)
+# features = sp.csr_matrix(feature_matrix)
+features = sp.csr_matrix(new_emb)
+
 labels = encode_onehot(api_dataframe['ServiceClassification'])
 # features = normalize(features)
 idx_train = TrainIndex
@@ -226,7 +225,7 @@ cuda = torch.cuda.is_available()
 fastmode = False
 seed = 42
 epochs = 3000  # 2000
-lr = 0.01
+lr = 0.001
 # weight_decay = 5e-4
 weight_decay = 0
 hidden = [1024, 1024]  # [2048, 1024]    # [256, 128]
